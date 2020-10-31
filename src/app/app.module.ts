47ad824756476795ca414cce08c4c43762ab00d3
Mahsa Paziraei <mahsa_paziraei@yahoo.com>
2020-10-31 12:28:08 +0330

    Shopping list added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent
   ],
   imports: [
     BrowserModule
