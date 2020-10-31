00d29a63c2065f1832d6d56d631cf6d3b1f0763b
Ava <avalazemzadeh@gmail.com>
2020-10-31 17:50:10 +0330

    init

---
+++ src/app/item.model.ts
@@ -0,0 +1,12 @@
+export class ItemModel {
+
+    constructor(
+        public id: number,
+        public phoneNumber: string,
+        public type: boolean,
+        public cost: number,
+        public status: boolean,
+        public operator: string) {
+
+    }
+}
