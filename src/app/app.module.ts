11890ca5cf2084b62203cfb24c00282466fedf5d
Mahsa Paziraei <mahsa_paziraei@yahoo.com>
2020-10-31 17:55:05 +0330

    ie kari

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import {ShoppingCardComponent} from './shopping-card/shopping-card.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShoppingListComponent,
-    ShoppingCardComponent
+    ShoppingCardComponent,
+    FilterComponent
 
   ],
   imports: [
