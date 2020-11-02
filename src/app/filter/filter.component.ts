a1249fcaed5f052a6c6086aca53b716d0eaad386
Mahsa Paziraei <mahsa_paziraei@yahoo.com>
2020-11-02 16:27:44 +0330

    filter

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,5 +1,6 @@
+import { SimDataService } from './../services/sim-data.service';
+import { ItemModel } from './../item.model';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
@@ -7,9 +8,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilterComponent implements OnInit {
 
-  constructor() { }
+  filter: FilterModel[] = [];
+
+
+  constructor(private dataService: SimDataService) { }
+
+  addFilter(model: FilterModel) {
+    this.filter.push(model);
+  }
+
+  removeFilter(model: FilterModel) {
+    this.filter = this.filter.filter(item => !(item.fieldName === model.fieldName && item.value === model.value));
+  }
+
+  onTypeClick(event) {
+    const newFilter: FilterModel = { fieldName: event.target.dataset.name, value: event.target.dataset.value };
+    if (event.target.checked) {
+      this.addFilter(newFilter);
+    } else {
+      this.removeFilter(newFilter);
+    }
+    this.dataService.filterResults(this.filter);
+  }
+
+  onRangeClick(event) {
+    let newFilter: FilterModel;
+    if (event.target.dataset.minvalue) {
+      newFilter = { fieldName: event.target.dataset.name, value: event.target.dataset.minvalue, type: 'min' };
+      if (event.target.checked) {
+        this.addFilter(newFilter);
+      } else {
+        this.removeFilter(newFilter);
+      }
+    }
+    if (event.target.dataset.maxvalue) {
+      newFilter = { fieldName: event.target.dataset.name, value: event.target.dataset.maxvalue, type: 'max' };
+      if (event.target.checked) {
+        this.addFilter(newFilter);
+      } else {
+        this.removeFilter(newFilter);
+      }
+    }
+    this.dataService.filterResults(this.filter);
+  }
 
   ngOnInit() {
   }
+}
 
+export interface FilterModel {
+  fieldName: string;
+  value: string | number | boolean;
+  type?: string;
 }
