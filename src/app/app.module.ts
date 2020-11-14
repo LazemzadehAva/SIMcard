a50a6d213fc5fb43cb8edfe7c74c262dc0c9887b
Mahsa Paziraei <mahsa_paziraei@yahoo.com>
2020-11-14 10:25:51 +0330

    undo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,28 @@
+import { SearchBoxComponent } from './search-box/search-box.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { LocalStorageService } from './services/local-storage.service';
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { PublicModule } from './public/public.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-
+    HeaderComponent,
+    FooterComponent,
+    SearchBoxComponent,
   ],
   imports: [
     FormsModule,
     BrowserModule,
     AppRoutingModule,
+    SharedModule,
+    PublicModule,
   ],
   providers: [LocalStorageService],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,30 +1,22 @@
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RouterModule } from '@angular/router';
 import { SliderModule } from 'primeng/slider';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SearchBoxComponent } from './search-box/search-box.component';
 import { FilterComponent } from './filter/filter.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { FormsModule } from '@angular/forms';
-import { LayoutComponent } from './layout/layout.component';
 
 
 @NgModule({
   declarations: [
     ShoppingListComponent,
-    FooterComponent,
-    HeaderComponent,
-    SearchBoxComponent,
     FilterComponent,
-    LayoutComponent,
-    RouterModule
   ],
   imports: [
     CommonModule,
     SliderModule,
-    FormsModule
+    FormsModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
