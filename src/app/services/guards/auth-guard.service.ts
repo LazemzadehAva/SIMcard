61435493e498d6c369e36a03573978ba8ed91bee
Mahsa Paziraei <mahsa_paziraei@yahoo.com>
2020-11-14 12:35:51 +0330

    guard added

--- src/app/services/guards/auth-guard.service.ts
+++ src/app/services/guards/auth-guard.service.ts
@@ -4,15 +4,16 @@ import {
     RouterStateSnapshot,
     Router
 } from '@angular/router';
-import { AuthService } from './auth.service';
+import { AuthService } from '../auth.service';
+
 
 export class AuthGaurd implements CanActivate {
     constructor(private router: Router, private authservice: AuthService) { }
     // tslint:disable-next-line: max-line-length
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | import('@angular/router').UrlTree | import('rxjs').Observable<boolean | import('@angular/router').UrlTree> | Promise<boolean | import('@angular/router').UrlTree> {
-        if (!this.authservice.isAdmin()) {
-            this.router.navigate(['error403']);
-            this.router.navigate(['error404']);
+
+        if (!this.authservice.isAuthenticated()) {
+            this.router.navigate(['login']);
             return false;
         }
         return true;
