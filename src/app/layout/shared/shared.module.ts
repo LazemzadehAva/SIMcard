644eb37dc3d1f4da6fe3574cb037a7e72b832f17
Ava <avalazemzadeh@gmail.com>
2020-11-17 16:38:24 +0330

    merge

--- src/app/layout/shared/shared.module.ts
+++ src/app/layout/shared/shared.module.ts
@@ -1,7 +1,6 @@
 import { SharedRoutingModule } from './shared-routing.module';
 import { SharedComponent } from './shared.component';
 import { AdminModule } from './admin/admin.module';
-import { RouterModule } from '@angular/router';
 import { SliderModule } from 'primeng-lts/slider';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -10,12 +9,11 @@ import { SplitButtonModule } from 'primeng-lts/splitbutton';
 import { FilterComponent } from './filter/filter.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import {DynamicDialogModule} from 'primeng/dynamicdialog';
-import { AddNewSimComponent } from './admin/add-new-sim/add-new-sim.component';
 @NgModule({
   declarations: [
     ShoppingListComponent,
     FilterComponent,
-    SharedComponent
+    SharedComponent,
   ],
   imports: [
     CommonModule,
@@ -28,7 +26,7 @@ import { AddNewSimComponent } from './admin/add-new-sim/add-new-sim.component';
 
   ],
   entryComponents: [
-    AddNewSimComponent
+
   ]
 })
 export class SharedModule { }
